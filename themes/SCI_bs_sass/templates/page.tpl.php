60a72e0a388997ba57603e3a82ca3433d96f263a
aleverenz <leverenz@uta.edu>
2018-01-03 17:01:00 -0600

    add html template

--- themes/SCI_bs_sass/templates/page.tpl.php
+++ themes/SCI_bs_sass/templates/page.tpl.php
@@ -75,28 +75,25 @@
 ?>
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
   <div class="<?php print $container_class; ?>">
-     <!--
-	<div class="navbar-header">
-      <?php //if ($logo): ?>
-        <a class="logo navbar-btn pull-left" href="<?php //print $front_page; ?>" title="<?php //print t('Home'); ?>">
-          <img src="<?php //print $logo; ?>" alt="<?php //print t('Home'); ?>" />
+    <div class="navbar-header">
+      <?php if ($logo): ?>
+        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
         </a>
-      <?php //endif; ?>
+      <?php endif; ?>
 
-      <?php //if (!empty($site_name)): ?>
-        <a class="name navbar-brand" href="<?php //print $front_page; ?>" title="<?php //print t('Home'); ?>"><?php //print $site_name; ?></a>
-      <?php //endif; ?>
+      <?php if (!empty($site_name)): ?>
+        <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+      <?php endif; ?>
 
       <?php //if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      
-	   <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
           <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-		-->
-      <?php //endif; ?>
+      <?php endif; ?>
 	 
     </div>
 
