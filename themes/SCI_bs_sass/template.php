a8de3f9c7a1d437cb92f18fc410bda3b04611d4d
aleverenz <leverenz@uta.edu>
2018-01-23 15:03:31 -0600

    update placeholder text

--- themes/SCI_bs_sass/template.php
+++ themes/SCI_bs_sass/template.php
@@ -9,9 +9,9 @@
 function SCI_bs_sass_form_views_exposed_form_alter(&$form, &$form_state, $form_id) {
  if (isset($form['search_api_views_fulltext'])) {
  	// Add ARIA role 'search' to the form
-	$form['search_api_views_fulltext']['#attributes']['role'] = 'search';
+	$form['search_api_views_fulltext']['#attributes']['role'] = 'search SCI archive';
 	// Alternative (HTML5) placeholder attribute instead of using the javascript
-    $form['search_api_views_fulltext']['#attributes']['placeholder'] = t('search');
+    $form['search_api_views_fulltext']['#attributes']['placeholder'] = t('search SCI archive');
 
   }
 }
