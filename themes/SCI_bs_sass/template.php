89b9406ea82ba40eb2c4866862986e5903b30b89
aleverenz <leverenz@uta.edu>
2018-01-23 14:59:54 -0600

    update form template

--- themes/SCI_bs_sass/template.php
+++ themes/SCI_bs_sass/template.php
@@ -7,7 +7,7 @@
 * hook_form_FORM_ID_alter
 */
 function SCI_bs_sass_form_views_exposed_form_alter(&$form, &$form_state, $form_id) {
- if (isset($form['search_api_multi_fulltext'])) {
+ if (isset($form['search_api_views_fulltext'])) {
  	// Add ARIA role 'search' to the form
 	$form['search_api_views_fulltext']['#attributes']['role'] = 'search';
 	// Alternative (HTML5) placeholder attribute instead of using the javascript
