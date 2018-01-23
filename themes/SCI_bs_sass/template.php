446bcad7b0edb6311988d9cd29deed815ee24930
aleverenz <leverenz@uta.edu>
2018-01-23 14:40:50 -0600

    update form template

--- themes/SCI_bs_sass/template.php
+++ themes/SCI_bs_sass/template.php
@@ -6,7 +6,7 @@
 /**
 * hook_form_FORM_ID_alter
 */
-function SCI_bs_sass_form_views_exposed_form_alter(&$form, &$form_state, $form_id) {
+function SCI_bs_sass_form_views-exposed-form-search-page-page_alter(&$form, &$form_state, $form_id) {
  if (isset($form['search_api_multi_fulltext'])) {
  	// Add ARIA role 'search' to the form
 	$form['search_api_views_fulltext']['#attributes']['role'] = 'search';
