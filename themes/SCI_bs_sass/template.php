100e97eb89da47d1d05f36868438620085e29167
aleverenz <leverenz@uta.edu>
2018-01-23 14:32:27 -0600

    update form css and form template to add placeholder

--- themes/SCI_bs_sass/template.php
+++ themes/SCI_bs_sass/template.php
@@ -3,3 +3,15 @@
  * @file
  * The primary PHP file for this theme.
  */
+/**
+* hook_form_FORM_ID_alter
+*/
+function sci_form_views_exposed_form_alter(&$form, &$form_state, $form_id) {
+ if (isset($form['search_api_multi_fulltext'])) {
+ 	// Add ARIA role 'search' to the form
+	$form['search_api_multi_fulltext']['#attributes']['role'] = 'search';
+	// Alternative (HTML5) placeholder attribute instead of using the javascript
+    $form['search_api_multi_fulltext']['#attributes']['placeholder'] = t('search');
+
+  }
+}
