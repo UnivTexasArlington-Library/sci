91797791bc946291911f755e47fae465c7a7c9b1
aleverenz <leverenz@uta.edu>
2018-01-24 10:52:11 -0600

    sci logo

--- themes/SCI_bs_sass/templates/page--front.tpl.php
+++ themes/SCI_bs_sass/templates/page--front.tpl.php
@@ -77,9 +77,7 @@
   <div class="<?php print $container_class; ?>">
     <div class="navbar-header">
       <?php if ($logo): ?>
-        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
+         <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       <?php endif; ?>
 
       <?php if (!empty($site_name)): ?>
