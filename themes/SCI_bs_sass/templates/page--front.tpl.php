edd9c78c08ed4cbf67d9c6330ec364e335fb4350
aleverenz <leverenz@uta.edu>
2018-01-25 08:24:43 -0600

    update teaser bg

--- themes/SCI_bs_sass/templates/page--front.tpl.php
+++ themes/SCI_bs_sass/templates/page--front.tpl.php
@@ -116,9 +116,9 @@
     <?php endif; ?>
   </div>
 </header>
-
-<div class="main-container <?php print $container_class; ?>">
 <div class="teaser-bg"></div>
+<div class="main-container <?php print $container_class; ?>">
+
   <header role="banner" id="page-header">
     <?php if (!empty($site_slogan)): ?>
       <p class="lead"><?php print $site_slogan; ?></p>
